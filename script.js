a0e0910c507c2cd8c4a5e1aff34a981e277177de
Joseluis Loza <luis@Joseluiss-MacBook-Pro.local>
2023-07-27 19:21:34 -0700

    added js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function clickFunction() {
+        window.open('https://www.amazon.com/gp/video/detail/B00BQPJE7S/ref=atv_hm_vid_c_vJb2Rv_1_1', '_blank')
+}
+
+function clickFunction2() {
+    window.open('https://www.hbo.com/movies/500-days-of-summer')
+}
+
+function clickFunction3() {
+    window.open('https://www.amazon.com/gp/video/detail/amzn1.dv.gti.04a9f72c-f9c8-c4d1-f97b-6d7ad986a51f?autoplay=0&ref_=atv_cf_strg_wb')
+}
+function clickFunction4() {
+    window.open('https://www.amazon.com/gp/video/detail/amzn1.dv.gti.f47d27e9-9077-4f5e-915c-067740d37be7?autoplay=0&ref_=atv_cf_strg_wb')
+}
+function clickFunction5() {
+    window.open('https://www.amazon.com/gp/video/detail/amzn1.dv.gti.0ea9f746-562d-2416-7aa1-31e7c3deb7b0?autoplay=0&ref_=atv_cf_strg_wb')
+}
+function clickFunction6() {
+    window.open('https://pluto.tv/en/on-demand/movies/601dbf9287be07001a29e6e6?utm_medium=textsearch&utm_source=google')
+}
+function clickFunction7() {
+    window.open("https://pluto.tv/en/on-demand/movies/5e83ac701fa6a9001bb9df24?utm_medium=textsearch&utm_source=google")
+}
+function clickFunction8() {
+    window.open("https://www.amazon.com/gp/video/detail/amzn1.dv.gti.4a437bc5-eb90-43df-bb75-127897245aaf?autoplay=0&ref_=atv_cf_strg_wb")
+}
+function clickFunction9() {
+    window.open('https://www.amazon.com/gp/video/detail/amzn1.dv.gti.3351a4fb-de71-4238-a363-a9640cdc4889?autoplay=0&ref_=atv_cf_strg_wb')
+}
